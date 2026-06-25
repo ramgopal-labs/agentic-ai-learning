ab6759ad775a2d7bbfdce2e6476cf00cd6f90c75
ramgopal-labs <patidar.ramgopal8345@gmail.com>
2026-06-25 15:02:03 +0530

    Add Test class with greeting functionality

---
+++ src/testfile.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Claude!");
+        greet("Ram");
+    }
+
+    public static void greet(String name) {
+        System.out.println("Welcome, " + name + "!");
+    }
+}
